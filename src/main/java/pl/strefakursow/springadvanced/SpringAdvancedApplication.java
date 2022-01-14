2a702caca1c6cffc512d7166165136137c1d452d
Artur <percikartur@gmail.com>
2022-01-14 13:38:43 +0100

    Task Schedule in Spring.

--- src/main/java/pl/strefakursow/springadvanced/SpringAdvancedApplication.java
+++ src/main/java/pl/strefakursow/springadvanced/SpringAdvancedApplication.java
@@ -2,7 +2,9 @@ package pl.strefakursow.springadvanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SpringAdvancedApplication {
     public static void main(String[] args) {
---
+++ src/main/java/pl/strefakursow/springadvanced/component/CustomTaskScheduler.java
@@ -0,0 +1,19 @@
+package pl.strefakursow.springadvanced.component;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
+public class CustomTaskScheduler {
+
+//    @Scheduled(fixedRate = 5000)
+//    @Scheduled(fixedDelay = 5000)
+//    second, minuta, godzina, dzień miesiąca, miesiąc, dzień tygodnia
+    @Scheduled(cron = "${cron.expression}")
+    public void doSomeStuff() throws InterruptedException {
+//        Thread.sleep(1000);
+        System.out.println("Some operation, time: " + LocalTime.now());
+    }
+}
